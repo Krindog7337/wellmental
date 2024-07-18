38900294d412ef7907585d9c80281f755b1c1f1d
Krindog7337 <igstkrisna2006@gmail.com>
2024-07-18 20:48:09 +0800

    tw should be working rn

--- index.php
+++ index.php
@@ -1,26 +1,21 @@
-<?php 
-  include "content.php";
-  include "connection.php";
+<?php
+include "content.php";
+include "connection.php";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="src/css/output.css">
 
-    <title>Wellmental</title>
-  </head>
-  <body>
-    <nav class="navbar">
-      <a class="navbar-brand" href="#">WellMental</a>
-    </nav>
-    <div class="container-fluid">
-      <?php include $content ?>
-    </div>
-    <div class="bg-slate-400">terra</div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/output.css">
+  <title>Wellmental</title>
+</head>
 
-    <script src=""></script>
-  </body>
+<body>
+  <nav class="bg-red-200"></nav>
+  <?php include $content ?>
+</body>
+
 </html>
