5b666df7ffdf4cea1401482e8f2e758fa1d8a8ba
krindog666 <widiaaryani07@gmail.com>
2024-07-19 10:04:41 +0800

    template maybe

---
+++ content/quiz1.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/output.css">
+</head>
+<body>
+    <main>
+        <form action="">
+            <div class="q1">
+                <label for="q1">Lorem ipsum dolor sit amet.?</label>
+                <div class="radios">
+                    <span class="">
+                        <label for="radio_up0ri9ayuy"><input data-v-0b8a5d46="" aria-label="I strongly agree" id="radio_up0ri9ayuy" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="-3">
+                        <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> 
+                        <span data-v-0b8a5d46="" class="radio__label">I strongly agree <!----> </span>
+                    </label> <!----></span><span data-v-0b8a5d46="" class="sp-radio radio--opt-hl7kb size--56 color--green" data-v-3a580c6f="">
+                        <label data-v-0b8a5d46="" for="radio_yk7irlm34r">
+                            <input data-v-0b8a5d46="" aria-label="I moderately agree" id="radio_yk7irlm34r" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="-2"> 
+                            <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> 
+                            <span data-v-0b8a5d46="" class="radio__label">I moderately agree <!----> </span></label> <!----></span>
+                            <span data-v-0b8a5d46="" class="sp-radio radio--opt-zc3v5 size--46 color--green" data-v-3a580c6f=""><label data-v-0b8a5d46="" for="radio_fmp09nszbq"><input data-v-0b8a5d46="" aria-label="I agree" id="radio_fmp09nszbq" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="-1"> <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> <span data-v-0b8a5d46="" class="radio__label">I agree <!----> </span></label> <!----></span><span data-v-0b8a5d46="" class="sp-radio radio--opt-wggp8 size--36 color--neutral" data-v-3a580c6f=""><label data-v-0b8a5d46="" for="radio_mtutli8yra"><input data-v-0b8a5d46="" aria-label="I am not sure" id="radio_mtutli8yra" name="name_vy1f6aqeda" tabindex="0" data-focus-first="true" type="radio" value="0"> <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> <span data-v-0b8a5d46="" class="radio__label">I am not sure <!----> </span></label> <!----></span><span data-v-0b8a5d46="" class="sp-radio radio--opt-l6xxb size--46 color--purple" data-v-3a580c6f=""><label data-v-0b8a5d46="" for="radio_zfcxp3pw26"><input data-v-0b8a5d46="" aria-label="I disagree" id="radio_zfcxp3pw26" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="1"> <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> <span data-v-0b8a5d46="" class="radio__label">I disagree <!----> </span></label> <!----></span><span data-v-0b8a5d46="" class="sp-radio radio--opt-y08eu size--56 color--purple" data-v-3a580c6f=""><label data-v-0b8a5d46="" for="radio_v53l2wtuec"><input data-v-0b8a5d46="" aria-label="I moderately disagree" id="radio_v53l2wtuec" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="2"> <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> <span data-v-0b8a5d46="" class="radio__label">I moderately disagree <!----> </span></label> <!----></span><span data-v-0b8a5d46="" class="sp-radio radio--opt-agx1w size--70 color--purple" data-v-3a580c6f=""><label data-v-0b8a5d46="" for="radio_z1fg2riq5t"><input data-v-0b8a5d46="" aria-label="I strongly disagree" id="radio_z1fg2riq5t" name="name_vy1f6aqeda" tabindex="-1" type="radio" value="3"> <span data-v-0b8a5d46="" class="radio__tick"> <!----></span> <span data-v-0b8a5d46="" class="radio__label">I strongly disagree <!----> </span></label> <!----></span></div>
+                    
+                </div>
+            </div>
+        </form>
+    </main>
+</body>
+</html>
